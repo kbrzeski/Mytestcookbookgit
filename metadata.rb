368d76c30553556675f7073a68cad328cfeb1ae8
Krzysztof Brzeski <krzysztof.brzeski@gmail.com>
2014-11-27 14:07:11 +0100

    Pierwszy commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mytest-cookbook'
+maintainer       'kb_chef'
+maintainer_email 'krzysztof.brzeski@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures mytest-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
